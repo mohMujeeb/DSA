683f64dc2f687109f61d617c3ae6d611b03118a6
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-09-04 16:22:15 +0500

    revised

--- 03. Questions/GFG/reverseWord.py
+++ 03. Questions/GFG/reverseWord.py
@@ -1,10 +1,27 @@
+########################################
+"""You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+Example 2:
+
+Input:
+s = for
+Output: rof"""
+########################################
+
+
 def reverseWord(s):
-         revlist = list(s)
-         left = 0
-         right = len(s)-1
-         while left < right:
-             revlist[left], revlist[right] = revlist[right], revlist[left]
-             left += 1
-             right -= 1
-         return ("".join(revlist))
+    dup = s
+    revlist = list(s)
+    left = 0
+    right = len(s)-1
+    while left < right:
+        revlist[left], revlist[right] = revlist[right], revlist[left]
+        left += 1
+        right -= 1
+    return ("".join(revlist))
 print(reverseWord("Mujeeb"))
