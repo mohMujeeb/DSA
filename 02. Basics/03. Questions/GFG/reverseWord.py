7d783e2e78099009681723ff5ffe4daecc275899
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-09-04 16:12:54 +0500

    optimised

--- 02. Basics/03. Questions/GFG/reverseWord.py
+++ 02. Basics/03. Questions/GFG/reverseWord.py
@@ -15,12 +15,13 @@ Output: rof"""
 
 
 def reverseWord(s):
-         revlist = list(s)
-         left = 0
-         right = len(s)-1
-         while left < right:
-             revlist[left], revlist[right] = revlist[right], revlist[left]
-             left += 1
-             right -= 1
-         return ("".join(revlist))
+    dup = s
+    revlist = list(s)
+    left = 0
+    right = len(s)-1
+    while left < right:
+        revlist[left], revlist[right] = revlist[right], revlist[left]
+        left += 1
+        right -= 1
+    return ("".join(revlist))
 print(reverseWord("Mujeeb"))
