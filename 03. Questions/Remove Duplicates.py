c99c7b495517a6717affdb471727a8724131773e
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-09-18 11:23:52 +0500

    update

---
+++ 03. Questions/Remove Duplicates.py
@@ -0,0 +1,21 @@
+"""
+Given an integer array sorted in non-decreasing order, remove the duplicates in place such that each
+unique element appears only once. The relative order of the elements should be kept the same.If there
+are k elements after removing the duplicates, then the first k elements of the array should hold the 
+final result. It does not matter what you leave beyond the first k elements.
+
+Note: Return k after placing the final result in the first k slots of the array.
+"""
+def removeDuplicates(arr):
+    i = 0
+    for j in range(1, len(arr)):
+        if arr[i] != arr[j]:
+            i += 1
+            arr[i] = arr[j]
+    return i + 1
+
+arr = [1, 1, 2, 2, 2, 3, 3]
+k = removeDuplicates(arr)
+print("The array after removing duplicate elements is ")
+for i in range(k):
+    print(arr[i], end=" ")
