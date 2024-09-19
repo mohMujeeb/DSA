783004a2fcd169e39d0b4d69440c0eb97ce16481
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-09-19 20:55:12 +0500

    update

--- 03. Questions/Remove Duplicates.py
+++ 03. Questions/Remove Duplicates.py
@@ -7,15 +7,13 @@ final result. It does not matter what you leave beyond the first k elements.
 Note: Return k after placing the final result in the first k slots of the array.
 """
 def removeDuplicates(arr):
-    i = 0
-    for j in range(1, len(arr)):
-        if arr[i] != arr[j]:
-            i += 1
-            arr[i] = arr[j]
-    return i + 1
+    unique_list = []
+    unique_dict = {}
+    for value in arr:
+        if value not in unique_dict:
+            unique_dict[value] = True
+            unique_list.append(value)
+    return unique_list 
 
 arr = [1, 1, 2, 2, 2, 3, 3]
-k = removeDuplicates(arr)
-print("The array after removing duplicate elements is ")
-for i in range(k):
-    print(arr[i], end=" ")
+print(removeDuplicates(arr))
