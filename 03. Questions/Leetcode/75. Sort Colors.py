4b78cad20940d522ffd93a3573db4458b3429b81
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-09-24 21:33:02 +0500

    Updaate

--- 03. Questions/Leetcode/75. Sort Colors.py
+++ 03. Questions/Leetcode/75. Sort Colors.py
@@ -14,7 +14,37 @@ Input: nums = [2,0,1]
 Output: [0,1,2]
 
 Solved using Dutch National Flag Algorithm 
+
+Time Complexity Better Approach: O(N) + O(N)
+Space Complexity Better Approach: O(1)
+
+Time Complexity Optimal Approach: O(N)
+Space Complexity Optimal Approach: O(1)
 """
+
+#Better Approach
+def count_approach(nums):
+    cnt1 = 0
+    cnt2 = 0
+    cnt3 = 0
+    
+    for num in nums:
+        if num == 0:
+            cnt1 += 1
+        elif num == 1:
+            cnt2 += 1
+        else:
+            cnt3 += 1
+    
+    for i in range(cnt1):
+        nums[i] = 0
+    for i in range(cnt1, cnt2):
+        nums[i] = 1
+    for i in range(cnt2, cnt3):
+        nums[i] = 2
+    return nums
+   
+#Optimal Approach 
 def sortColors(nums):
         n = len(nums)
         low = 0
@@ -35,8 +65,17 @@ def sortColors(nums):
             
 test_case1 = [2, 0, 2, 1, 1, 0]
 test_case2 = [2, 0, 1]
-print("Sorted Colors:")
+
+#Sort Colors Optimal Approach
+print("Sort Colors Optimal")
 print("For Test Case 1:")
 print(sortColors(test_case1))
 print("For Test Case 2:")
 print(sortColors(test_case2))
+
+#Sort Colors Better Approach
+print("Sort Colors Better Approach")
+print("For Test Case 1:")
+print(count_approach(test_case1))
+print("For Test Case 2:")
+print(count_approach(test_case2))
