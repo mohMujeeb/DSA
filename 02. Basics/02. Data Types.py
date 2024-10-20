b9d65837e5e0b128a6a74ce2632c1c11633c310b
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-10-21 01:21:30 +0500

    update

---
+++ 02. Basics/02. Data Types.py
@@ -0,0 +1,26 @@
+"""
+Data Types in Python 
+ - Integer (int store decimal numbers) -> int
+ - Float   (float store decimal numbers in points) -> float
+ - Boolean (bool store boolean values as true or false) -> bool
+ - String  (string store sequence of characters) -> str
+ 
+"""
+
+
+integer_dt = 8
+float_dt = 16.2
+boolean_dt = True
+string_dt = "abc"
+
+print("Integer: ", integer_dt)
+print("Float: ", float_dt)
+print("Boolean: ", boolean_dt)
+print("String: ", string_dt)
+
+# Another way to check Data Types in Python is using type function 
+
+print("Type of integer_dt: ", type(integer_dt))
+print("Type of float_dt: ", type(float_dt))
+print("Type of boolean_dt: ", type(boolean_dt))
+print("Type of string_dt: ", type(string_dt))
