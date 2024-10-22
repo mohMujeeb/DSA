aee2d988e1c0400466d34194a83045bfd7b9a0b0
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-10-22 18:46:28 +0500

    update

---
+++ 05. OOPs/01. Class and Object.py
@@ -0,0 +1,26 @@
+# Class is a blueprint for creating objects 
+
+# Lets create student class and print student data
+class Student:   # Blue Print
+    name = "Shushant"       # Data 
+    
+# Lets create instance/object of student class to access student data
+obj = Student()     # Instance/Object
+print(obj.name)     
+
+
+# Lets create a Car class with some data i.e: color, brand, model 
+
+class Car:
+    color = "Red"
+    brand = "Toyota"
+    model = "Corolla"
+    
+# Lets create an instance/object of Car class to access car data
+car = Car()
+print("Car Name: ",car.color)
+print("Car Brand: ",car.brand)
+print("Car Name: ",car.model)
+
+
+
