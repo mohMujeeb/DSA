76b023e7d4d6b77380f71934499c28ba396c424d
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-11-19 00:32:39 +0500

    update

--- 05. OOPs/02. Attributes.py
+++ 05. OOPs/02. Attributes.py
@@ -5,9 +5,42 @@ is shared among all ths instances of that class. It remains the same for every
 instance of the class
 """
 
-class Student:
-    name = "kuch-bi"
-    age = 10
+class Employee:
+    #class attributes
+    empCount = 0
     
-obj = Student()
-print("Name:", obj.name,"Age:", obj.age)
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+        # modifying class attribute
+        Employee.empCount += 1
+        
+        # accessing class attribute
+        print("Name: ", self.__name, "Age: ", self.__age)
+        print("Total Employee: ", Employee.empCount, "\n")
+        
+    
+# Object 
+e1 = Employee("Mujeeb", 21)
+print()
+e2 = Employee("Saif", 24)
+
+
+# Instance Attribute
+
+""" 
+Instance Attribute is a variable that is specific to an individual object of class
+It is defined inside the __init__() method
+"""
+
+class Person:
+    def __init__(self, name, age):
+        self.__name = name 
+        self.__age = age
+        
+        print("Name:", self.__name, "Age:", self.__age)
+        
+# object
+p1 = Person("John", 25)
+print()
+p2 = Person("David", 30)
