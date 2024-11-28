52d974e489522d5a12b58cee1ce486e96c199879
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-11-28 17:24:01 +0500

    update

---
+++ 03. Questions/GFG160/Arrays/Second Largest Element.py
@@ -0,0 +1,33 @@
+"""Given an array of positive integers arr[], return the second largest element
+from the array. If the second largest element doesn't exist then return -1.
+Note: The second largest element should not be equal to the largest element.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest 
+element is 34.
+"""
+def getSecondLargest(arr):
+    n = len(arr)
+    
+    if n < 2:
+        return -1
+    
+    large = arr[0]
+    for i in range(n):
+        if arr[i] > large:
+            large = arr[i]
+    
+    s_large = -1
+    for i in range(n):
+        if arr[i] > s_large and arr[i] < large:
+            s_large = arr[i]
+            
+            
+    return s_large
+
+arr = [12, 11, 13, 5, 6, 7]
+second_largest = getSecondLargest(arr)  
+print("Second largest element is:", second_largest)
