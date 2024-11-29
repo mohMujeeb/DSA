1f2cfae454b6b6268eae2fad2f6099fe2182c5df
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-11-29 22:24:18 +0500

    update

--- 03. Questions/GFG160/Arrays/Move All Zeros Ro End Of Array.py
+++ 03. Questions/GFG160/Arrays/Move All Zeros Ro End Of Array.py
@@ -1,8 +1,19 @@
+""" 
+Given an array arr[]. Push all the zeros of the given array to the right end 
+of the array while maintaining the order of non-zero elements. Do the mentioned
+change in the array in place.
+
+Examples:
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.
+"""
+# [Naive Approach] Using Temporary Array - O(n) Time and O(n) Space
 def moveAllZeroToEnd(arr):
     n = len(arr)
     
     # create temp array 
-    
     temp = [0] * n
     j = 0 
     
@@ -22,8 +33,24 @@ def moveAllZeroToEnd(arr):
         arr[i] = temp[i]
         
     return arr
+# [Expected Approach] One Traversal - O(n) Time and O(1) Space
+
+def pushZerosToEnd(arr):
+    
+    n = len(arr)
+    count = 0
+    for i in range(n):
+        if arr[i] != 0:
+            arr[i], arr[count] = arr[count], arr[i]
+            count += 1
+            
+    return arr
 
 arr = [1, 2, 0, 4, 3, 0, 5, 0]
 
-output = moveAllZeroToEnd(arr)
-print(output)
+naive = moveAllZeroToEnd(arr)
+expected = pushZerosToEnd(arr)
+
+
+print(naive)
+print(expected)
