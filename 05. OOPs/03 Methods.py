6f3745a09e53c08eb22d5004037c4a906b6a7462
Mujeeb Ur Rehman <118097643+mohMujeeb@users.noreply.github.com>
2024-12-01 03:59:29 +0500

    update

--- 05. OOPs/03 Methods.py
+++ 05. OOPs/03 Methods.py
@@ -13,54 +13,43 @@
 
 class Employee:
     empCount = 0
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        Employee.empCount += 1
     
-    def showcount(self):
-        print(self.empCount)
-    # syntax -> classmethod(instance_method)   
-    counter = classmethod(showcount)
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+        Employee.empCount += 1
+    def show_count(self):
+        return self.empCount
+    #Syntax -> classmethod(instance_method)
+    counter = classmethod(show_count)
     
 e1 = Employee("Mujeeb", 21)
-e2 = Employee("Saif", 24)
-
-print(e1.showcount)
-print()
-print(Employee.counter)
+print("Employee Count: ",Employee.counter())
 
 
-# Using @classmethod Decorator
-""" 
-More convenient than declaring a instance method and then transforming it into 
-class method
-"""
-
-#Syntax @classmethod
-"""
-@classmethod
-def show()
-    more...
-
-"""
+# @classmethod
 
 class Student:
-    std_count = 0
+    stdCount = 0
     
-    def __init__(self, name, subject):
+    def __init__(self, name, age):
         self.__name = name
-        self.__subject = subject
-        
-        Student.std_count += 1
-        
+        self.__age = age
+        Student.stdCount += 1
+    
     @classmethod
     def show_count(cls):
-        print(cls.std_count)
-        
-    @classmethod
-    def new_std(self, name, subject):
-        pass
+        return cls.stdCount
     
+    @classmethod
+    def new_std(cls, name, age):
+        return cls(name, age)
+    
+e1 = Student("John", 19)
+s2 = Student.new_std("Mujeeb", 21)
+print("Student Count: ",Student.show_count())   
+
+
+
 
         
