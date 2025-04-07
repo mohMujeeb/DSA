d69de76e753c46e2451ed5f208a07cb9d15290ea
mohMujeeb <moh.mujeeb@outlook.com>
2025-04-07 14:59:22 +0500

    Matrix

--- 06. Linked List/01. Linked List in Python.py
+++ 06. Linked List/01. Linked List in Python.py
@@ -3,18 +3,25 @@ class Node:
         self.data = data 
         self.next = None
 
-
-node1 = Node(5)
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def linkedListPrint(self):
+        currentNode = self.head
+        while currentNode is not None:
+            print(currentNode.data)
+            currentNode = currentNode.next
+node1 = LinkedList()
+node1.head = Node(5)
 node2 = Node(10)
 node3 = Node(15)
 node4 = Node(20)
 
-node1.next = node2
+
+node1.head.next = node2
 node2.next = node3
 node3.next = node4
 
-currentNode = node1
-while currentNode:
-    print(currentNode.data, end="->")
-    currentNode = currentNode.next
-print("null")
+print("Linked List:")
+print(node1.linkedListPrint)
+
